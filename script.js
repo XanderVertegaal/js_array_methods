dc4bd8c82a85244d98d9e7418b419c123508eb0d
Xander Vertegaal <xander.vertegaal@xs4all.nl>
2021-10-04 12:13:24 +0200

    Finished exercises.

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-const array = [
-    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
-    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
-    { name: "A. Curry", profession: "kikvorsman", age: 32 },
-    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
-    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
-    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
-  ];
-  
-  for (let person of array) {
-    //hier moeten console.logs komen
-    console.log('Hele persoon:', person)
-    console.log('Naam:', person.name)
-    console.log('Geboortejaar', 2021 - person.age)
-    if (person.age > 50) {
-        console.log('Deze persoon is ouder dan 50.')
-    } else {
-        console.log('Deze persoon is jonger dan 50.')
-    }
-    // console.log(person.age > 50 ? 'Deze persoon is ouder dan 50.' : 'Deze persoon is jonger dan 50.')
-    console.log(person.name, 'is een', person.profession)
-    console.log('\n')
+// Oefening A
+const addTheWordCool = function(array){
+  array.push('cool');
+  return array
+}
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+// resultaat: ["nice", "awesome", "tof", "cool"]
+
+// Oefening B
+const amountOfElementsInArray = function(array) {
+  return array.length
+}
+console.log('Totale lengte:', amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+// 3
+
+// Oefening C
+const selectBelgiumFromBenelux = function(array) {
+  return array[0]
+  // return array.splice(0,1);
+}
+console.log('Het eerste land in BeNeLux is:', selectBelgiumFromBenelux(["België", "Nederland", "Luxemburg"])); 
+// resultaat: "België" 
+
+// Oefening D
+const lastElementInArray = function(array) {
+  return array.pop()
+  // return array.at(-1)
+  // return array.reverse()[0]
+}
+console.log('Het laatste dier is:', lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"])); 
+  // resultaat: "Schildpad"
+
+
+// Oefening E
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+const impeachTrumpSlice = function(array) {
+    return array.slice(1,)
   }
+  const impeachTrumpSplice = function(array) {
+        array.splice(0, 1,)
+        return array
+  }
+  console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]     Slice muteert de array niet.
+  console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]    Splice muteert de array wel.
+
+// Oefening F
+const stringsTogether = function(array) {
+   return array.join(' ')
+}
+console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}'])) 
+//resultaat: "Winc Academy is leuk ;-}"
+
+// Oefening G
+const combineArrays = function(array1, array2) {
+  return array1.concat(array2)
+}
+console.log(combineArrays([1,2,3], [4,5,6]))
+// resultaat: [1,2,3,4,5,6]
